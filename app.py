f502526654fe604ad3fa2adbb8a5a8b967082be4
Ivan <Ivan@ivan.ru>
2016-12-07 21:10:44 +0000

    Добавлен клиент для произведения манипуляций с графом, осуществлена тестовая отправка дынных для обработки на стороне сервера.

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-#
-test
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('network.html', title='Главная')
+
+@app.route('/data/', methods=['POST'])
+def data():
+    name=request.form['start']
+    pasw=request.form['finish']
+    return render_template('data.html', name=name, pasw=pasw, title='Данные')
